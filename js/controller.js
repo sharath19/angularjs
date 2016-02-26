9fc7e18cc75afd3063088f9d93c7fb09ae5912fc
sharath19 <reddy.sharath19@gmail.com>
2016-02-25 23:15:58 -0500

    Delete controller.js

--- js/controller.js
+++
@@ -1,8 +0,0 @@
-var myApp= angular.module('myApp',[]);
-myApp.controller('MyController',function MyController($scope){
-   $scope.author = {
-       'name':'Sharath',
-       'title': 'Web Developer',
-       'company': 'Alden Image'
-   } ;
-});
